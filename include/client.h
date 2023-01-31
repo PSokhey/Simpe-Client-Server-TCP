dea35f78b37627c4eb77d32311d2f5586daf64dc
prabh <prabhjeetsokhey@gmail.com>
2023-01-30 23:27:26 -0500

    client side has basic functions implemented but not tested.

--- include/client.h
+++ include/client.h
@@ -1,9 +1,17 @@
 //
 // Created by prabh on 1/30/23.
 //
-
 #ifndef DC_SHELL_CLIENT_H
 #define DC_SHELL_CLIENT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
 #define MAX_BUFFER_SIZE 4096
 #define PORT 5000
--- src/client.c
+++ src/client.c
@@ -1,22 +1,17 @@
 //
 // Created by prabh on 1/30/23.
 //
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
+
 #include "client.h"
 
 
 void runClient(char* ipAddress, char* fileName) {
 
     int socketClient; //socket file descriptor for connection.
+    int fileFD;
     struct sockaddr_in serverAddr; // address to server.
     char buffer[MAX_BUFFER_SIZE];
-    int bytesRead;
+    int bytesRead = 0;
 
     // creating the socket.
     socketClient = socket(AF_INET, SOCK_STREAM, 0);
@@ -39,7 +34,32 @@ void runClient(char* ipAddress, char* fileName) {
         exit(1);
     }
 
+    // open the file
+    fileFD = open(fileName, O_RDONLY);
+    if(fileFD < 0) {
+        perror("Could not open file.");
+        exit(1);
+    }
 
+    // send file to the server.
+    while ((bytesRead = read(fileFD, buffer, MAX_BUFFER_SIZE)) > 0) {
+        if (send(socketClient, buffer, bytesRead, 0) < 0) {
+            perror("send");
+            exit(1);
+        }
+    }
+
+    // close file descriptor when done.
+    close(fileFD);
+
+    // clear buffer for getting a response from server.
+    memset(buffer, 0, MAX_BUFFER_SIZE);
+
+    // read response from server and display to user.
+    if(recv(socketClient, buffer, MAX_BUFFER_SIZE,0) < 0) {
+        perror("Could not read response from user.");
+        exit(1);
+    }
 
     // Close socket before exiting program.
     close(socketClient);

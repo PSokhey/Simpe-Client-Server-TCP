c68818db2d7cb03129595aa78328fcfdc930c112
prabh <prabhjeetsokhey@gmail.com>
2023-01-30 20:56:15 -0500

    added server files to cmake and changed logic in main.

---
+++ include/server.h
@@ -0,0 +1,8 @@
+//
+// Created by prabh on 1/30/23.
+//
+
+#ifndef DC_SHELL_SERVER_H
+#define DC_SHELL_SERVER_H
+
+#endif //DC_SHELL_SERVER_H
--- src/main.c
+++ src/main.c
@@ -8,15 +8,37 @@
 int main(int argc, char *argv[])
 {
 
+    // Check to see if the first argument is null.
     if (argv[1] == NULL) {
         fprintf(stderr, "Error: first argument MUST be 'client' or 'server'.\n");
         return 1;
     }
 
-    // check if specified client or server first.
+    // check if the program should run as a client or server.
     if(strcmp(argv[1], "server") != 0 && strcmp(argv[1], "client") != 0) {
         perror("Must be server or client\n");
         return 1;
     }
+
+    // Check if the client has enough arguments to run.
+    if(strcmp(argv[1], "client") == 0 && argc == 4)
+    {
+    //TODO: IMPLIMENT CLIENT CODE.
+    printf("client ran");
+    }
+
+    // Chec if the server has enough arguments to run.
+    else if(strcmp(argv[1], "server") == 0 && argc == 3)
+    {
+        //TODO: IMPLIMENT SERVER CODE.
+        printf("server ran");
+    }
+    else {
+        perror("Client MUST have the file to read and Ip of server.\n");
+        return 1;
+
+    }
+
+
     return 0;
 }

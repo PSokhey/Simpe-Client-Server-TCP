cc52c48ff9e5d4d94bb5470112b564364624bb81
prabh <prabhjeetsokhey@gmail.com>
2023-01-30 20:37:39 -0500

    program switches checks if should run as a server or client.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-    printf("Hello, World!\n");
+
+    if (argv[1] == NULL) {
+        fprintf(stderr, "Error: first argument MUST be 'client' or 'server'.\n");
+        return 1;
+    }
+
+    // check if specified client or server first.
+    if(strcmp(argv[1], "server") != 0 && strcmp(argv[1], "client") != 0) {
+        perror("Must be server or client\n");
+        return 1;
+    }
     return 0;
 }
